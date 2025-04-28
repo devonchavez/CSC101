8393f35515bb36a9cd0d78b179a15ef025859f65
Devon Chavez <130639276+p0yson@users.noreply.github.com>
2025-04-28 14:31:00 -0700

    project 2 now working

--- Project2/RestaurantManager/src/RestaurantCheckManager.java
+++ Project2/RestaurantManager/src/RestaurantCheckManager.java
@@ -3,6 +3,6 @@ import java.util.Scanner;
 public class RestaurantCheckManager {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
+        System.out.println(input);
     }
 }
